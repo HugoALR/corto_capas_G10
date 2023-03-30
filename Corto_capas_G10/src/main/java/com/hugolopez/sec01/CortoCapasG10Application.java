0bba492be25e1295a93a6f452f0f7adbb2e0ff25
HugoALR <00005719@uca.edu.sv>
2023-03-29 19:21:31 -0600

    first commit

---
+++ Corto_capas_G10/src/main/java/com/hugolopez/sec01/CortoCapasG10Application.java
@@ -0,0 +1,13 @@
+package com.hugolopez.sec01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CortoCapasG10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CortoCapasG10Application.class, args);
+	}
+
+}
---
+++ Corto_capas_G10/src/test/java/com/hugolopez/sec01/CortoCapasG10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugolopez.sec01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CortoCapasG10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
